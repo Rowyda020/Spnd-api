ea5bddf810f984576e9de3c6d6e4a19727f99fa4
Rowyda020 <rowydaramadan@gmail.com>
2026-01-28 18:56:33 +0200

    feature:update totalIncome when the user add a new income

--- src/models/user.mjs
+++ src/models/user.mjs
@@ -11,6 +11,10 @@ const userSchema = new Schema({
         type: String,
         required: false
     },
+    totalIncome: {
+        type: Number,
+        default: 0.0
+    },
     email: {
         type: String,
         unique: true,
--- src/routes/income.mjs
+++ src/routes/income.mjs
@@ -8,19 +8,24 @@ import {
     matchedData,
 } from "express-validator";
 import Income from "../models/income.mjs";
+import User from "../models/user.mjs";
 const router = Router()
 router.post('/create-income', checkSchema(createIncomeValidationSchema), isLoggedIn, async (req, res) => {
     const result = validationResult(req)
     if (!result.isEmpty()) return res.status(400).send(result.array());
     const data = matchedData(req);
+    console.log(req.user)
     const incomeData = {
         ...data,
         user: req.user._id
     };
 
+    const updatedIncome = data.amount + req.user.totalIncome
     const newIncome = new Income(incomeData);
+    const updateUserTotalIncome = await User.findById(req.user._id)
     try {
         const savedIncome = await newIncome.save();
+        await updateUserTotalIncome.updateOne({ totalIncome: updatedIncome })
         const userIncomeResponse = savedIncome.toObject();
         return res.status(201).send(userIncomeResponse);
     } catch (error) {
